dfd03d49b3b3b0e16b15cc6975930d73b255bf1e
WhalesBob <ndw8200@naver.com>
2023-01-20 14:56:07 +0900

    feat(CarRacingGame) : 게임 시작 기능 초기화

---
+++ src/main/java/racingcar/CarRacingGame.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import racingcar.views.InputFeature;
+import racingcar.views.OutputFeature;
+
+public class CarRacingGame {
+
+    private InputFeature input;
+    private OutputFeature output;
+
+    public CarRacingGame(InputFeature input, OutputFeature output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public void playGame(){
+
+    }
+}
